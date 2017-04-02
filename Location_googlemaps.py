673f3bb40ed17ea7e9d5ca17752a969af7a2cc2a
dushyantRathore <dushyant.bgs@gmail.com>
2017-04-02 17:13:42 +0530

    Get your Location using google maps API

---
+++ Location_googlemaps.py
@@ -0,0 +1,36 @@
+import googlemaps
+import requests
+import json
+
+send_url = 'http://freegeoip.net/json'
+r = requests.get(send_url)
+j = json.loads(r.text)
+lat = j['latitude']
+lon = j['longitude']
+
+print "Latitude : " + str(lat)
+print "Longitude : " + str(lon)
+
+gmaps = googlemaps.Client(key='AIzaSyC8Ny7X4cuCOd_9SE21raopnTC-KswEs0Y')
+API_key = "AIzaSyC8Ny7X4cuCOd_9SE21raopnTC-KswEs0Y"
+
+# Request
+url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + str(lat) + "," + str(lon) + "&key=" + str(API_key)
+x = requests.get(url)
+resp = json.loads(x.text)
+
+l = len(resp["results"][0]["address_components"])
+
+# print l
+
+for i in range(0,l):
+    if resp["results"][0]["address_components"][i]["types"][0] == "administrative_area_level_1":
+        index = i
+        break
+
+print "Your location : " + resp["results"][0]["address_components"][index]["long_name"]
+
+
+
+
+
