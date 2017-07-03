46a67e785843541c843ecdbda35680a42ecdb550
dushyantRathore <dushyant.bgs@gmail.com>
2017-07-03 15:57:48 +0530

    Send messsage on Gmail

---
+++ Gmail_send_mail.py
@@ -0,0 +1,22 @@
+import smtplib
+
+
+username = raw_input("Enter your EmailID : ")
+password = raw_input("Enter your password : ")
+receiver = raw_input("Enter the Email ID of the receiver : ")
+
+# creates SMTP session
+s = smtplib.SMTP('smtp.gmail.com', 587)
+
+# start TLS for security
+s.starttls()
+
+# Authentication
+s.login(username, password)
+
+msg = raw_input("Enter the message : ")
+
+
+s.sendmail(username, receiver, msg=msg)
+
+s.quit()
