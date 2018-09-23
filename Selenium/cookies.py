fc1c19500c77199417aaccb6bd86702cc7c4eb23
dushyantRathore <dushyant.bgs@gmail.com>
2018-09-23 20:46:10 +0530

    Updated

--- Selenium/cookies.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-import time
-
-driver = webdriver.Chrome("C:/Users/39400/Desktop/chromedriver") # Link to the downloaded chrome driver
-driver.get("https://google.com/")  # URL for navigation
-
-print(driver.get_cookies())
-
-driver.close()
